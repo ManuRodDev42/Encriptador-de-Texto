7019857684153bf24df2b4f3aad6e3ebacb0df7c
Manuel Rodríguez <manuelalejandrorod4220@gmailcom>
2024-08-27 19:02:17 -0500

    enciptacion y desencriptacion en JS

--- app.js
+++ app.js
@@ -23,3 +23,29 @@ function hacerCifrado(){
         alert("Debes ingresar algún texto");
     }
 }
+
+function hacerDescifrado(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+        .replace(/ai/gi, "a")
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    if (texto.length !=0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "/desencriptar.png";
+    }else{
+        muñeco.src = "/muñeco-alura.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+}
